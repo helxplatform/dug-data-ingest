8fd23a4c86a75e053388ca49dba5e5c132d349cf
Gaurav Vaidya <gaurav@ggvaidya.com>
2024-08-06 13:33:29 -0400

    Undebugged bash script.

--- scripts/bdc/ingest.sh
+++ scripts/bdc/ingest.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
 # Bash strict mode
-set -euxo pipefail
-#IFS=$'\n\t'
+set -euo pipefail
+
+# But not the IFS change, because we're not using arrays and it
+# messes up RCLONE_FLAGS.
 
 # A script for ingesting data from BDC into LakeFS.
 echo Started ingest from BDC at `date`.
@@ -15,7 +17,7 @@ python bdc/get_bdc_studies_from_gen3.py /data/bdc_dbgap_ids.csv
 
 # Step 2. Download the dbGaP XML files from BDC.
 mkdir -p /data/bdc
-# python bdc/get_dbgap_data_dicts.py /data/bdc_dbgap_ids.csv --format CSV --field "Accession" --outdir /data/bdc --group-by Program
+python bdc/get_dbgap_data_dicts.py /data/bdc_dbgap_ids.csv --format CSV --field "Accession" --outdir /data/bdc --group-by Program
 
 # Step 3. Upload the dbGaP XML files to BDC.
 echo Uploading dbGaP XML files to LakeFS using Rclone.
@@ -33,24 +35,22 @@ export RCLONE_CONFIG_LAKEFS_NO_CHECK_BUCKET=true
 LAKEFS_REPOSITORY="bdc-test4"
 
 # Sync (https://rclone.org/commands/rclone_sync/)
-RCLONE_FLAG_LIST=("--progress" "--track-renames" "--no-update-modtime")
-RCLONE_FLAGS="${RCLONE_FLAG_LIST[@]}"
-
+RCLONE_FLAGS="--progress --track-renames --no-update-modtime"
+# --progress: Display progress.
 # --track-renames: If a file exists but has only been renamed, record that on the destination.
 # --no-update-modtime: Don't update the last-modified time if the file is identical.
 
 touch /data/bdc/test.txt
-rclone sync "/data/" "lakefs:$LAKEFS_REPOSITORY/main/test/" $RCLONE_FLAGS
-#rclone sync "/data/bdc/BioLINCC/" "lakefs:$LAKEFS_REPOSITORY/main/BioLINCC/" $RCLONE_FLAGS
-#rclone sync "/data/bdc/COVID19/" "lakefs:$LAKEFS_REPOSITORY/main/COVID19/" $RCLONE_FLAGS
-#rclone sync "/data/bdc/DIR/" "lakefs:$LAKEFS_REPOSITORY/main/DIR/" $RCLONE_FLAGS
-#rclone sync "/data/bdc/imaging/" "lakefs:$LAKEFS_REPOSITORY/main/imaging/" $RCLONE_FLAGS
-#rclone sync "/data/bdc/LungMAP/" "lakefs:$LAKEFS_REPOSITORY/main/LungMAP/" $RCLONE_FLAGS
-#rclone sync "/data/bdc/NSRR/" "lakefs:$LAKEFS_REPOSITORY/main/NSRR/" $RCLONE_FLAGS
-#rclone sync "/data/bdc/parent/" "lakefs:$LAKEFS_REPOSITORY/main/parent/" $RCLONE_FLAGS
-#rclone sync "/data/bdc/PCGC/" "lakefs:$LAKEFS_REPOSITORY/main/PCGC/" $RCLONE_FLAGS
-#rclone sync "/data/bdc/RECOVER/" "lakefs:$LAKEFS_REPOSITORY/main/RECOVER/" $RCLONE_FLAGS
-#rclone sync "/data/bdc/topmed/" "lakefs:$LAKEFS_REPOSITORY/main/topmed/" $RCLONE_FLAGS
+rclone sync "/data/bdc/BioLINCC/" "lakefs:$LAKEFS_REPOSITORY/main/BioLINCC/" $RCLONE_FLAGS
+rclone sync "/data/bdc/COVID19/" "lakefs:$LAKEFS_REPOSITORY/main/COVID19/" $RCLONE_FLAGS
+rclone sync "/data/bdc/DIR/" "lakefs:$LAKEFS_REPOSITORY/main/DIR/" $RCLONE_FLAGS
+rclone sync "/data/bdc/imaging/" "lakefs:$LAKEFS_REPOSITORY/main/imaging/" $RCLONE_FLAGS
+rclone sync "/data/bdc/LungMAP/" "lakefs:$LAKEFS_REPOSITORY/main/LungMAP/" $RCLONE_FLAGS
+rclone sync "/data/bdc/NSRR/" "lakefs:$LAKEFS_REPOSITORY/main/NSRR/" $RCLONE_FLAGS
+rclone sync "/data/bdc/parent/" "lakefs:$LAKEFS_REPOSITORY/main/parent/" $RCLONE_FLAGS
+rclone sync "/data/bdc/PCGC/" "lakefs:$LAKEFS_REPOSITORY/main/PCGC/" $RCLONE_FLAGS
+rclone sync "/data/bdc/RECOVER/" "lakefs:$LAKEFS_REPOSITORY/main/RECOVER/" $RCLONE_FLAGS
+rclone sync "/data/bdc/topmed/" "lakefs:$LAKEFS_REPOSITORY/main/topmed/" $RCLONE_FLAGS
 
 # Report errors.
 echo Downloads complete at `date`.
