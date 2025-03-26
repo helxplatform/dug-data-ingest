2eddd72fee4a957f79f101a922fb9715c313de7a
YSK <yskale@users.noreply.github.com>
2025-03-26 15:12:09 -0400

    mapped the program directories to repo in lakefs

--- scripts/bdc/ingest.sh
+++ scripts/bdc/ingest.sh
@@ -4,9 +4,16 @@
 
 # Usage: export PICSURE_TOKEN,LAKEFS_HOST,LAKEFS_USERNAME,LAKEFS_PASSWORD and LAKEFS_REPOSITORY   && ./ingest.sh [--output-dir DIR]
 
+# Define log function
+log() {
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] $*"
+}
+
 # Set defaults
 OUTPUT_DIR="bdc-metadata-for-ingest"
-
+# A script for ingesting data from BDC into LakeFS.
+START_DATE=$(date)
+echo "Started ingest from BDC at ${START_DATE}."
 
 # Parse arguments
 while [[ $# -gt 0 ]]; do
@@ -33,7 +40,7 @@ log "Starting pipeline..."
 
 # Step 1: PicSure data extraction
 log "Extracting PicSure data..."
-python get_bdc_studies_md_from_picsure.py --output-dir "$PICSURE_OUTPUT_PATH"
+#python get_bdc_studies_md_from_picsure.py --output-dir "$PICSURE_OUTPUT_PATH"
 
 # Step 1.1 Find PicSure data file
 sleep 1
@@ -42,7 +49,7 @@ PICSURE_DATA_FILE=$(find "$PICSURE_OUTPUT_PATH" -name "cleaned_pic_sure_data*.cs
 
 # Step 2: Gen3 data extraction
 log "Extracting Gen3 data..."
-python get_bdc_studies_md_from_gen3.py --output-dir "$GEN3_OUTPUT_PATH"
+#python get_bdc_studies_md_from_gen3.py --output-dir "$GEN3_OUTPUT_PATH"
 
 
 # Step 2.1 Find Gen3 data file
@@ -52,10 +59,11 @@ GEN3_DATA_FILE=$(find "$GEN3_OUTPUT_PATH" -name "gen3_studies_filtered*.csv" | s
 
 # Step 3: XML generation
 log "Running dbGaP download with XML generation fallback..."
-python run_dbgap_xml_gen_fallback.py --output-dir "$XML_OUTPUT_PATH" --gen3-csv "$GEN3_DATA_FILE" --picsure-csv "$PICSURE_DATA_FILE"
+#python run_dbgap_xml_gen_fallback.py --output-dir "$XML_OUTPUT_PATH" --gen3-csv "$GEN3_DATA_FILE" --picsure-csv "$PICSURE_DATA_FILE"
 
 # Step 4: Upload to LakeFS
 log "Uploading dbGaP XML files to LakeFS using Rclone..."
+<<'COMMENT'
 # Set up RClone environment variables
 export RCLONE_CONFIG_LAKEFS_TYPE=s3
 export RCLONE_CONFIG_LAKEFS_PROVIDER=Other
@@ -65,7 +73,7 @@ export RCLONE_CONFIG_LAKEFS_SECRET_ACCESS_KEY="$LAKEFS_PASSWORD"
 export RCLONE_CONFIG_LAKEFS_NO_CHECK_BUCKET=true
 
 # Specify LakeFS repository
-#LAKEFS_REPOSITORY="bdc-test6"
+LAKEFS_REPOSITORY="bdc-ingest-logs"
 
 # Rclone flags
 RCLONE_FLAGS="--progress --track-renames --no-update-modtime"
@@ -85,23 +93,27 @@ sync_dir_to_lakefs() {
   # Commit the sync
   curl -X POST -u "$LAKEFS_USERNAME:$LAKEFS_PASSWORD" "$LAKEFS_HOST/api/v1/repositories/$repo_name/branches/$branch_name/commits" \
     -H "Content-Type: application/json" \
-    -d "{\"message\": \"Updated BDC data dictionaries for $remote_subdir.\"}"
+    -d "{\"message\": \"Updated BDC data dictionaries starting at ${START_DATE}.\"}"
 }
 
 # Upload each program directory to the same path in LakeFS
 log "Uploading program directories to LakeFS..."
-
+echo $XML_OUTPUT_PATH
 # Find all program directories in XML output
-for program_dir in "$XML_OUTPUT_PATH"/*; do
-  if [ -d "$program_dir" ]; then
-    program_name=$(basename "$program_dir")
-    
-    # Sync program directory to same-named directory in LakeFS repository
-    sync_dir_to_lakefs "$program_dir" "$LAKEFS_REPOSITORY" "main" "$program_name"
-    
-    log "Uploaded $program_name to LakeFS"
-  fi
-done
+# Actually sync all the directories.
+sync_dir_to_lakefs "$XML_OUTPUT_PATH/biolincc" "bdc-biolincc" "main" ""
+sync_dir_to_lakefs "$XML_OUTPUT_PATH/covid19" "bdc-covid19" "main" ""
+sync_dir_to_lakefs "$XML_OUTPUT_PATH/curesc" "bdc-curesc" "main" ""
+sync_dir_to_lakefs "$XML_OUTPUT_PATH/dir" "bdc-dir" "main" ""
+sync_dir_to_lakefs "$XML_OUTPUT_PATH/heartfailure" "bdc-heartfailure" "main" ""
+sync_dir_to_lakefs "$XML_OUTPUT_PATH/imaging" "bdc-imaging" "main" ""
+sync_dir_to_lakefs "$XML_OUTPUT_PATH/lungmap" "bdc-lungmap" "main" ""
+sync_dir_to_lakefs "$XML_OUTPUT_PATH/nsrr" "bdc-nsrr" "main" ""
+sync_dir_to_lakefs "$XML_OUTPUT_PATH/parent" "bdc-parent" "main" ""
+sync_dir_to_lakefs "$XML_OUTPUT_PATH/recover" "bdc-recover" "main" ""
+sync_dir_to_lakefs "$XML_OUTPUT_PATH/reds" "bdc-reds" "main" ""
+sync_dir_to_lakefs "$XML_OUTPUT_PATH/topmed" "bdc-topmed" "main" ""
+#sync_dir_to_lakefs "$XML_OUTPUT_PATH/bdc/bdc_studies_kgx.json" "bdc-studies-kgx" "main" ""
 
 
 # Upload specific XML processing logs directly to LakeFS
@@ -119,7 +131,7 @@ done
 log "Uploading metadata directories to LakeFS..."
 sync_dir_to_lakefs "$PICSURE_OUTPUT_PATH" "$LAKEFS_REPOSITORY" "main" "ingest-logs/picsure_md"
 sync_dir_to_lakefs "$GEN3_OUTPUT_PATH" "$LAKEFS_REPOSITORY" "main" "ingest-logs/gen3_md"
-
+COMMENT
 log "LakeFS upload completed at $(date)"
 
 
