3157f041b3c40e6c3326e367c91c097dc9821d73
Gaurav Vaidya <gaurav@ggvaidya.com>
2025-04-09 11:48:37 -0600

    Added shellcheck to run-linters.sh.

--- run-linters.sh
+++ run-linters.sh
@@ -16,4 +16,8 @@
 # Python linting: Ruff (https://github.com/astral-sh/ruff)
 PYTHON_DIR="${BASEDIR:-.}"
 ruff check --select I --fix ${PYTHON_DIR}
-[ -z "${FIX}" ] && ruff format ${PYTHON_DIR} 
+[ -z "${FIX}" ] && ruff format ${PYTHON_DIR}
+
+# Shell scripts: Shellcheck (https://github.com/koalaman/shellcheck)
+# You can fix these files by asking shellcheck to generate a diff (`-f diff`) and then piping that to `patch`.
+find "${BASEDIR:-.}" -path "./venv" -prune -o -name '*.sh' -print0 | xargs -0 shellcheck
